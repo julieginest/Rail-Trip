7e035ac2e3601ca69b05444bfa73b4112065792a
julienginest <julien.ginestiere+git@gmail.com>
2024-09-02 16:22:25 +0200

    Settings -> ajout des données BDD depuis .env

--- App-RailTrip/App/App/settings.py
+++ App-RailTrip/App/App/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Récupération du .env
+load_dotenv("../../../.env")
+
+DATABASE_ADRESS = os.getenv('DATABASE_ADRESS')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
