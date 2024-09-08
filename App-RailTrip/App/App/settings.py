39b5a51b7a6e255fff33b70c98be844cea28077a
malika95290 <your_email@example.com>
2024-09-09 00:09:45 +0200

    ajout url

--- App-RailTrip/App/App/settings.py
+++ App-RailTrip/App/App/settings.py
@@ -120,4 +120,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Templates/static")]
 
 # Default primary key field type
-# https://docs.djangoprojec
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
