20e4775cb7a475cfc2c27f891fc37d03fb091e37
malika95290 <malika.amranimaam@gmail.com>
2024-12-11 17:53:19 +0100

    register (verifications erreurs, enregistré dans bdd OK

--- App-RailTrip/App/App/views/register.py
+++ App-RailTrip/App/App/views/register.py
@@ -1,18 +1,18 @@
-from django.views.generic import FormView
+from django.shortcuts import redirect
 from django.contrib import messages
+from django.views.generic import FormView
 from ..forms import RegisterForm
 
 class RegisterView(FormView):
     template_name = "app_users/register.html"
     form_class = RegisterForm
-    success_url = "/login/"
 
     def form_valid(self, form):
         user = form.save()
         messages.success(self.request, f"Bienvenue {user.pseudo} sur RailTrip")
-        return super().form_valid(form)
+        # Redirection vers la page du profil après succès
+        return redirect("profile")  # Assurez-vous que 'profile' est le nom du chemin vers votre page profile.html
 
     def form_invalid(self, form):
-        messages.error(self.request, "Le formulaire est invalide.")
-
+        messages.error(self.request, "Le formulaire contient des erreurs.")
         return super().form_invalid(form)
